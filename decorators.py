18b5c4c1319aaf9d8de6dac47a5d46aa197556c9
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2025-03-09 00:51:16 +0530

    feat: add singleton decorator

---
+++ decorators.py
@@ -0,0 +1,16 @@
+import functools
+
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def init_singleton(*args, **kwargs):
+        instance = init_singleton.__instance
+        print(instance, cls)
+
+        if instance is None:
+            init_singleton.__instance = cls(*args, **kwargs)
+        return init_singleton.__instance
+
+    # set this attribute beforehand to avoid attributeError when function is called
+    init_singleton.__instance = None
+    return init_singleton
