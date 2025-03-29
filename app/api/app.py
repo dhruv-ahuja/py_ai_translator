b48d24622a60a36aaa992bcb78a25b4790502382
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2025-03-30 02:19:28 +0530

    fix: prevent failure translating freshly crawled data

--- app/api/app.py
+++ app/api/app.py
@@ -18,10 +18,13 @@ async def translate(
     logger.debug("received translate request", input=req_input)
     url = req_input.url
     language = req_input.language
+    fresh_crawl = False
 
+    # TODO: move all conditional logic to service layer
     crawled_data = await get_crawled_data_by_url(url, async_session)
     if not crawled_data:
         crawled_data = await crawl_single_url(url, cache=req_input.cache)
+        fresh_crawl = True
     else:
         logger.info("found existing crawled data", id=crawled_data.id, url=url)
 
@@ -29,19 +32,20 @@ async def translate(
         logger.error("No content found for URL", url=url)
         return TranslateResponse(success=False, error={"message": "No content found for the given URL"}, data=None)
 
-    translation_output: AiTranslationOutput | None = await crawled_data.awaitable_attrs.translation_output
-    if translation_output:
-        logger.info("Found existing translation output", id=translation_output.id, url=url)
-        translated_content = translation_output.content
-        return TranslateResponse(
-            success=True,
-            error=None,
-            data={
-                "translation_id": crawled_data.id,
-                "content": translated_content,
-                "metadata": {"output_file_path": None, "crawled_metadata": crawled_data.crawled_metadata},
-            },
-        )
+    if not fresh_crawl:
+        translation_output: AiTranslationOutput | None = await crawled_data.awaitable_attrs.translation_output
+        if translation_output:
+            logger.info("Found existing translation output", id=translation_output.id, url=url)
+            translated_content = translation_output.content
+            return TranslateResponse(
+                success=True,
+                error=None,
+                data={
+                    "translation_id": crawled_data.id,
+                    "content": translated_content,
+                    "metadata": {"output_file_path": None, "crawled_metadata": crawled_data.crawled_metadata},
+                },
+            )
 
     translated_content = await translate_content(crawled_data, language)
     if not translated_content:
